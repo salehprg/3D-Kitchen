0c88effc521dfd1f379f14a82463c51b158442a7
saleh_prg <salehebrahimian@gmail.com>
2021-03-21 01:40:11 +0330

    Scale completed

--- Assets/Scripts/ScaleWithDrag.cs
+++ Assets/Scripts/ScaleWithDrag.cs
@@ -4,39 +4,86 @@ using UnityEngine;
 
 public class ScaleWithDrag : MonoBehaviour
 {
-    public Transform worldAnchor;
+    public Transform wall;
     public Vector3 initialScale;
-    public float CameraZDistance;
+    public Vector3 initialPos;
     public Camera mainCamera;
+    public Vector3 mouseWorldPos;
+
     void Start()
     {
-        initialScale = transform.localScale;
-        mainCamera = Camera.main;
-        CameraZDistance = mainCamera.WorldToScreenPoint(transform.position).z;
+        initialScale = wall.localScale;
+        initialPos = wall.position;
+        mainCamera = GameObject.FindGameObjectWithTag("2D Camera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(right != null)
         {
-            Vector3 mouseScreenPos = Input.mousePosition;
-            mouseScreenPos.z = CameraZDistance;
-
-            Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(mouseScreenPos);
-
-            float distance = Vector3.Distance(worldAnchor.position , mouseWorldPos);
-            transform.localScale = new Vector3(initialScale.x , distance / 2 , initialScale.z);
-
-            Vector3 middlePoint = (worldAnchor.position + mouseWorldPos) / 2;
-            transform.position = middlePoint;
-
-            Vector3 rotation = (mouseWorldPos - worldAnchor.position);
-            transform.up = rotation;
+            transform.position = new Vector3(right.position.x , transform.position.y , right.position.z);
         }
     }
 
-    private void OnMouseDrag() {
+    public Vector3 refrenceVector;
+    Transform right;
+    Transform left;
+    Vector3 startPos;
+    WallInfo wallInfo;
+    float dist;
+    private void OnMouseDown() {
+        startPos = GetMouseInWorld();
+        wallInfo = wall.GetComponent<WallInfo>();
+        right = wallInfo.Right.transform;
+        left = wallInfo.Left.transform;
+
+        wall.SetParent(wallInfo.Left.transform);
+        right.SetParent(wall);
+
+        dist = Vector3.Distance(right.position , left.position);
+    }
+
+    private void OnMouseUp() {
+        //wall.SetParent(wallInfo.MainParent.transform);
+    }
+
+    private void OnMouseDrag() 
+    {
+        Vector3 _right = right.position;
+        _right.y = 0;
+
+        Vector3 _left = left.position;
+        _left.y = 0;
+
+        refrenceVector = _right - _left;
         
+        mouseWorldPos = GetMouseInWorld();
+
+
+        float angleRot = Vector3.SignedAngle(refrenceVector , mouseWorldPos - _left , wall.up);
+        left.localEulerAngles += new Vector3(0 , angleRot , 0);
+
+        float dist2 = Vector3.Distance(_left , mouseWorldPos);
+        Debug.Log(dist2 / dist);
+
+        left.localScale = new Vector3(initialScale.x * dist2 / dist , initialScale.y , initialScale.z);
+        
+    }
+
+    Vector3 GetMouseInWorld()
+    {
+        Vector3 result = new Vector3();
+
+        Vector3 mousePos = Input.mousePosition;
+        Ray ray = mainCamera.ScreenPointToRay(mousePos);
+        RaycastHit hit = new RaycastHit();
+
+        if(Physics.Raycast(ray.origin , ray.direction , out hit))
+        {
+            result = new Vector3(hit.point.x , 0 , hit.point.z);
+        }
+
+        return result;
     }
 }
---
+++ Assets/Scripts/WallInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallInfo : MonoBehaviour
+{
+    public GameObject MainParent;
+    public GameObject Right;
+    public GameObject Left;
+
+    private void Start() {
+        MainParent = transform.parent.gameObject;
+    }
+}
